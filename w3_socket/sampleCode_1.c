a3f239ff82f596acfc00d8c4e566e39771baefa0
Celine10811020 <86948988+Celine10811020@users.noreply.github.com>
2022-09-26 22:45:01 +0800

    Update sampleCode_1.c

--- w3_socket/sampleCode_1.c
+++ w3_socket/sampleCode_1.c
@@ -12,39 +12,45 @@ lseek：可以改變當前檔案偏移量。*/
 
 int main()
 {
-char read_file[] = "/home/imx/shares/open/read.bmp";
-char write_file[] = "/home/imx/shares/open/write.bmp";
-int file_len = 0;
-int file_total = 0;
-int read_len = 0;
-char buf[1024] = {0};
-int read_fd = open(read_file,O_RDWR);
-int write_fd = open(write_file,O_RDWR|O_TRUNC |O_CREAT,00777);
-if(read_fd < 0 || write_fd < 0 )
-{
-printf("file open error\n");
-return 1;
-}
-file_len = (int)lseek(read_fd,0,SEEK_END);//get file total size
-file_total = file_len;
-printf("file_total:%d\n",file_total);
-lseek(read_fd,0,SEEK_SET);
-printf("read start\n");
-while(read_len = read(read_fd,buf,1024))//file read
-{
-write(write_fd,buf,read_len);//file write
-file_len = file_len - read_len;
-memset(buf,0,1024);
-}
-printf("read and write end\n");
-if(file_len != 0)//check file read write
-{
-close(read_fd);
-close(write_fd);
-printf("error:read!=write\n");
-return 1;
-}
-close(read_fd);
-close(write_fd);
-return 0;
+  char read_file[] = "/home/imx/shares/open/read.bmp";
+  char write_file[] = "/home/imx/shares/open/write.bmp";
+  int file_len = 0;
+  int file_total = 0;
+  int read_len = 0;
+  char buf[1024] = {0};
+  int read_fd = open(read_file,O_RDWR);
+  int write_fd = open(write_file,O_RDWR|O_TRUNC |O_CREAT,00777);
+  
+  if(read_fd < 0 || write_fd < 0 )
+  {
+    printf("file open error\n");
+    return 1;
+  }
+  
+  file_len = (int)lseek(read_fd,0,SEEK_END);//get file total size
+  file_total = file_len;
+  printf("file_total:%d\n",file_total);
+  lseek(read_fd,0,SEEK_SET);
+  printf("read start\n");
+  
+  while(read_len = read(read_fd,buf,1024))//file read
+  {
+    write(write_fd,buf,read_len);//file write
+    file_len = file_len - read_len;
+    memset(buf,0,1024);
+  }
+  
+  printf("read and write end\n");
+  
+  if(file_len != 0)//check file read write
+  {
+    close(read_fd);
+    close(write_fd);
+    printf("error:read!=write\n");
+    return 1;
+  }
+  
+  close(read_fd);
+  close(write_fd);
+  return 0;
 }
