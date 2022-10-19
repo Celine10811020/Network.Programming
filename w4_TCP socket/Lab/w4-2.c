d86ce563549c8fd54db7f77f8d6cb476fe46d98d
Celine10811020 <86948988+Celine10811020@users.noreply.github.com>
2022-10-19 11:00:10 +0800

    Update w4-2.c

--- w4_TCP socket/Lab/w4-2.c
+++ w4_TCP socket/Lab/w4-2.c
@@ -1,59 +1,50 @@
-#include <stdio.h>      // for printf() and fprintf() 
-#include <sys/socket.h> // for socket(), connect(), send(), and recv() 
-#include <arpa/inet.h>  // for sockaddr_in and inet_addr() 
-#include <stdlib.h>     // for atoi() and exit() 
-#include <string.h>     // for memset() 
-#include <unistd.h>     // for close() 
+#include <stdio.h>      
+#include <sys/socket.h> 
+#include <arpa/inet.h>  
+#include <stdlib.h>     
+#include <string.h>     
+#include <unistd.h>     // usleep()
 
-#define STRBUFSIZE 1024 // Size of receive buffer 
-
-void DieWithError(char *errorMessage);  // Error handling function 
+#define STRBUFSIZE 16384
 
 int main(int argc, char *argv[])
 {
-    int sock;                        // Socket descriptor 
-    struct sockaddr_in echoServAddr; // Echo server address 
-    unsigned short echoServPort;     // Echo server port 
-    char *servIP;                    // Server IP address (dotted quad)
-    char echoString[STRBUFSIZE];     // Buffer for echo string  
-    char echoBuffer[STRBUFSIZE];     // Buffer for echo string 
-    unsigned int echoStringLen;      // Length of string to echo 
-    int bytesRcvd, totalBytesRcvd;   // Bytes read in single recv() and total bytes read 
-    int running = 1;                 // Check the loop state
+    int sock;                        
+    struct sockaddr_in echoServAddr; 
+    unsigned short echoServPort;     
+    char *servIP;                    
+    char echoString[STRBUFSIZE];       
+    char echoBuffer[STRBUFSIZE];        
+    int bytesRcvd;                    
 
-    if ( ( argc < 2 ) || (argc > 3 ) )    // Test for correct number of arguments 
+    if ( ( argc < 2 ) || (argc > 3 ) )    
     {
        fprintf( stderr, "Usage: %s <Server IP> [<Echo Port>]\n", argv[0] );
        exit(1);
     }
 
-    servIP = argv[1];             // First arg: server IP address (dotted quad) 
+    servIP = argv[1];             
 
     if ( argc == 3 )
-    {
-        // Use given port, if any 
+    { 
         echoServPort = atoi(argv[2]); 
     }
     else
     {
-        // 7 is the well-known port for the echo service
         echoServPort = 7;  
     } 
 
-    // Create a reliable, stream socket using TCP 
     if ( ( sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP ) ) < 0 )
     {
         perror("socket() failed");
         exit(1);
     }
     
-    // Construct the server address structure 
-    memset(&echoServAddr, 0, sizeof(echoServAddr));     // Zero out structure 
-    echoServAddr.sin_family      = AF_INET;             // Internet address family 
-    echoServAddr.sin_addr.s_addr = inet_addr(servIP);   // Server IP address 
-    echoServAddr.sin_port        = htons(echoServPort); // Server port 
+    memset(&echoServAddr, 0, sizeof(echoServAddr));     
+    echoServAddr.sin_family      = AF_INET;             
+    echoServAddr.sin_addr.s_addr = inet_addr(servIP);   
+    echoServAddr.sin_port        = htons(echoServPort); 
 
-    // Establish the connection to the echo server 
     if ( connect( sock, (struct sockaddr *) &echoServAddr, sizeof( echoServAddr ) ) < 0 )
     {
         perror( "connect() failed" );
@@ -61,58 +52,24 @@ int main(int argc, char *argv[])
     }
     
     if ( ( bytesRcvd = recv( sock, echoBuffer, STRBUFSIZE - 1, 0) ) <= 0 )
-        {
-            perror("recv() failed or connection closed prematurely");
-            exit(1);
-        }
-        printf( "%s", echoBuffer);
-    
-    int i;
-    for(i=0; i<1024; i++)
     {
-    	echoString[i] = i;
+        perror("recv() failed or connection closed prematurely");
+        exit(1);
     }
+    printf( "%s", echoBuffer);
     
-    while ( running )
+    
+    while (1) //wu qung hui quan
     {
         bzero( echoString, STRBUFSIZE );
         
-        send(sock, echoString, 1024, 0);
-        
-        
-        //bzero( echoBuffer, STRBUFSIZE );
-        
-        //printf("Enter(q for exit): \n"); 
-        
-        //fgets( echoString, STRBUFSIZE, stdin ); 
-        
-        //echoStringLen = strlen( echoString );
-        
-        /*if ( echoStringLen == 2 && echoString[0] == 'q' && echoString[1] == '\n' )
-        {
-            running = 0;
-            continue;
-        }*/
-        
-        /*if ( send(sock, echoString, echoStringLen, 0) != echoStringLen )
-        {
-            perror( "send() sent a different number of bytes than expected");
-            exit(1);
-        }*/
-        
-        //printf("Received"); 
-        
-        /*if ( ( bytesRcvd = recv( sock, echoBuffer, STRBUFSIZE - 1, 0) ) <= 0 )
-        {
-            perror("recv() failed or connection closed prematurely");
-            exit(1);
-        }*/
-        //printf( "(%d): ", bytesRcvd );
-        //printf( "%s", echoBuffer);      // Print the echo buffer 
-        
+        for(int i=0; i<16384; i++) //zhi zao wu yi yi de dong dong
+	{
+	    echoString[i] = 1;
+	}
+	usleep(15000);
+        send(sock, echoString, 16384, 0);       
     }
-    
-    printf("\n");    // Print a final linefeed 
 
     close(sock);
     return 0;
